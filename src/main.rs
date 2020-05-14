5a107a10cc4d95d8ca1dbd9cca1dc89cd6edfddc
yilmazgunalp <yilmazgunalp@gmail.com>
2020-05-14 21:47:25 +1000

    add 'add' subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::path::PathBuf;
+use std::process::Command;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(about = "Cronjob utility program.")]
+enum RustyCron {
+    Add {
+        #[structopt(parse(from_os_str))]
+        file: PathBuf,
+    },
+    Append {
+        job: String,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rcron: RustyCron = RustyCron::from_args();
+    match rcron {
+        RustyCron::Add { file } => add(&file),
+        _ => println!("something else"),
+    }
+}
+
+fn add(file: &PathBuf) {
+    Command::new("crontab")
+        .arg(file)
+        .output()
+        .expect("Failed to execute command");
 }
